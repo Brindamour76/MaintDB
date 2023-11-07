c1c3a54de37e7a6c3b70be05f7f0ca03ee588cf5
Brindamour76 <shane@kimandshane.com>
2023-11-07 16:56:06 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+	<head> 
+		<title>MaintDB</title>
+        <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/reset.css">
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/index.css">
+        
+
+    </head>
+	
+	<body>
+	<p>Hello, and welcome to Top Gear</p>
+	</body>
